121b8df85fe19ff9e47074c9515c1613671010b0
HMonique <161358207+HMonique@users.noreply.github.com>
2024-06-28 12:52:39 -0400

    gLab303.3.1

--- src/TestStringComparison3.java
+++ src/TestStringComparison3.java
@@ -4,9 +4,9 @@ public class TestStringComparison3 {
         String s2="Perscholas";
         String s3="Perschola";
         String s4="PerscholasX";
-        System.out.println(s1.compareTo(s2)); //0
-        System.out.println(s1.compareTo(s3)); // 1(because s1>s3)
-        System.out.println(s1.compareTo(s4)); // -1(because s1<s4 )
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s1.compareTo(s3));
+        System.out.println(s1.compareTo(s4));
     }
 }
 
