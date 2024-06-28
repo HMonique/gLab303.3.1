37f1a616773ecc29a1ea8ccbb2f418e2e216c1fd
HMonique <161358207+HMonique@users.noreply.github.com>
2024-06-27 22:57:27 -0400

    gLab303.3.1

--- src/StringTrimExample.java
+++ src/StringTrimExample.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class StringTrimExample {
+public class StringTrimExample {
+    public static void main(String args[]) {
+        String s1 = "  hello  ";
+        System.out.println(s1="how are you");
+        System.out.println(s1.trim()+"how are you");
+    }
 }
